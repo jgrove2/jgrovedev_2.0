f6af05f5eade2739f5453c1c7c3be052003de446
Jgrove <26723005+jgrove2@users.noreply.github.com>
2023-10-21 16:30:28 -0400

    Oops

--- src/handlers/TemplateHandler.go
+++ src/handlers/TemplateHandler.go
@@ -3,6 +3,6 @@ package handlers
 import "html/template"
 
 func HandleTemplates() *template.Template {
-	tmpl := template.Must(template.ParseGlob("templates/*"))
+	tmpl := template.Must(template.ParseGlob("src/templates/*"))
 	return tmpl
 }
