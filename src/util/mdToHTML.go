fb1a2eefbb66061b0654e6b45f37bea38ef2b804
Jgrove <26723005+jgrove2@users.noreply.github.com>
2023-10-21 20:56:15 -0400

    Markdown to html previewer

---
+++ src/util/mdToHTML.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
+)
+
+func MdToHTML(md []byte) []byte {
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock | parser.Mmark | parser.FencedCode
+	p := parser.NewWithExtensions(extensions)
+	doc := p.Parse(md)
+
+	// create HTML renderer with extensions
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	return markdown.Render(doc, renderer)
+}
